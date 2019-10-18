b45c18cacd4ab4647588a3d8d1b7a020f67bba2a
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-10-18 03:16:48 +0200

    added backup to firestore and delete

--- app/src/main/java/com/example/newshub/fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/newshub/fragments/FavoritesFragment.java
@@ -2,6 +2,7 @@ package com.example.newshub.fragments;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,11 +19,19 @@ import android.widget.Toast;
 
 import com.example.newshub.R;
 import com.example.newshub.adapter.FavoritesAdapter;
+import com.example.newshub.firebase.Firestore;
 import com.example.newshub.room.NewsItem;
 import com.example.newshub.room.NewsViewModel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.newshub.firebase.Firestore.SHARED_PREFS;
+import static com.example.newshub.firebase.Firestore.USERNAME;
+import static com.example.newshub.firebase.Firestore.collectionName;
+import static com.example.newshub.firebase.Firestore.passwordFieldName;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +42,17 @@ public class FavoritesFragment extends Fragment{
     private Context context;
     private ArrayList<NewsItem> newsItemArrayList;
     private FavoritesAdapter favoritesAdapter;
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private String password;
+    private String username;
+    private List<String> links;
+
+    private boolean loggedIn;
+
+    private FirebaseFirestore firebaseFirestore;
 
 
     public FavoritesFragment() {
@@ -55,9 +75,25 @@ public class FavoritesFragment extends Fragment{
         rv_favorites.setHasFixedSize(false);
 
 
+
+
         favoritesAdapter = new FavoritesAdapter(context, newsItemArrayList);
 
         rv_favorites.setAdapter(favoritesAdapter);
+
+        preferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+        password = preferences.getString(passwordFieldName, "");
+        username = preferences.getString(USERNAME, "");
+
+        if (!password.equals("")){
+            loggedIn = true;
+            firebaseFirestore = Firestore.getFirestore();
+        }
+        else {
+            loggedIn = false;
+        }
 
         newsViewModel = ViewModelProviders.of(getActivity()).get(NewsViewModel.class);
 
@@ -66,9 +102,13 @@ public class FavoritesFragment extends Fragment{
             public void onChanged(List<NewsItem> newsItems) {
                 //favoritesAdapter.setNewsItems((ArrayList<NewsItem>) newsItems);
                 newsItemArrayList.clear();
+                links = new ArrayList<>();
                 for (NewsItem item: newsItems){
                     Log.d("Item", item.getUrl());
                     newsItemArrayList.add(item);
+                    if (loggedIn){
+                        links.add(item.getUrl());
+                    }
                 }
                 favoritesAdapter.notifyDataSetChanged();
             }
@@ -76,9 +116,52 @@ public class FavoritesFragment extends Fragment{
         return view;
     }
 
-    /*@Override
-    public void onItemClick(int position) {
-        Toast.makeText(context, newsItemArrayList.get(position).getTitle(), Toast.LENGTH_SHORT).show();
-        newsViewModel.delete(newsItemArrayList.get(position));
-    }*/
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (loggedIn){
+            Item item = new Item(password, links);
+            firebaseFirestore
+                    .collection(collectionName)
+                    .document(username)
+                    .set(item)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(context, "Backup Updated.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    public class Item{
+        private String password;
+        private List<String> links;
+
+        public Item(String password, List<String> links) {
+            this.password = password;
+            this.links = links;
+        }
+
+        public Item(List<String> links) {
+            this.links = links;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public List<String> getLinks() {
+            return links;
+        }
+
+        public void setLinks(List<String> links) {
+            this.links = links;
+        }
+    }
 }
